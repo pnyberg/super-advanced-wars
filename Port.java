bb31ae3fa24a665b50adf13697dc8b2e651df731
Per Nyberg <per_skuru@msn.com>
2016-10-05 14:08:05 +0200

    Added Airport, factory and port-classes

---
+++ Port.java
@@ -0,0 +1,24 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+// === @TODO ====
+// * possibly add feature to remove units to be built
+// * define which units can be built in factory?
+public class Port extends Building {
+	public Port(int x, int y) {
+		super(x, y);		
+	}
+
+	public void paint(Graphics g) {
+		int paintX = x * tileSize;
+		int paintY = y * tileSize;
+
+		g.setColor(Color.white);
+		g.fillRect(paintX, paintY, tileSize, tileSize);
+		g.setColor(Color.black);
+		g.drawRect(paintX, paintY, tileSize, tileSize);
+
+		g.drawLine(paintX, paintY, paintX + tileSize, paintY + tileSize);
+		g.drawLine(paintX, paintY + tileSize, paintX + tileSize, paintY);
+	}
+}
