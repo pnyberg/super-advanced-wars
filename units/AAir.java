26149dba3779ea7a90a9bbbe69720899c2c85821
Per Nyberg <per_skuru@msn.com>
2016-10-20 11:25:26 +0200

    Added A-Air

---
+++ units/AAir.java
@@ -0,0 +1,73 @@
+package units;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class AAir extends Unit {
+	private static int price = 8000;
+	private static String typeName = "A-Air";
+
+	public AAir(int x, int y, Color color) {
+		super(x, y, color);
+
+		movement = 6;
+		movementType = Unit.BAND;
+		unitClass = Unit.VEHICLE;
+	}
+
+	public static void setPrice(int price) {
+		AAir.price = price;
+	}
+
+	public static int getPrice() {
+		return price;
+	}
+
+	public static String getTypeName() {
+		return typeName;
+	}
+
+	protected void paintUnit(Graphics g, int tileSize) {
+		int cx1 = x * tileSize + 3 * tileSize / 4;
+		int cy1 = y * tileSize + tileSize / 8;
+
+		int cx2 = x * tileSize + 7 * tileSize / 8;
+		int cy2 = y * tileSize + tileSize / 4;
+
+		int cx3 = x * tileSize + 3 * tileSize / 4 - 3;
+		int cy3 = y * tileSize + 7 * tileSize / 20 + 3;
+		int cx4 = x * tileSize + tileSize / 4 + 5;
+		int cy4 = y * tileSize + 7 * tileSize / 20 + 3;
+
+		int bodyWidth = 2 * tileSize / 5 + 1;
+		int bodyHeight = tileSize / 4 + 3;
+		int bodyAlignX = tileSize / 3 - 1;
+		int bodyAlignY = 7 * tileSize / 20 + 3;
+
+		// cannon
+		int[] cannonX = {cx1, cx2, cx3, cx4};
+		int[] cannonY = {cy1, cy2, cy3, cy4};
+		int npoints = 4;
+
+		if (active) {
+			g.setColor(color);
+		} else {
+			g.setColor(restingColor);
+		}
+		g.fillPolygon(cannonX, cannonY, npoints);
+
+		g.setColor(Color.black);
+		g.drawPolygon(cannonX, cannonY, npoints);
+
+		// body
+		if (active) {
+			g.setColor(color);
+		} else {
+			g.setColor(restingColor);
+		}
+		g.fillRect(x * tileSize + bodyAlignX, y * tileSize + bodyAlignY, bodyWidth, bodyHeight);
+
+		g.setColor(Color.black);
+		g.drawRect(x * tileSize + bodyAlignX, y * tileSize + bodyAlignY, bodyWidth, bodyHeight);
+	}
+}
