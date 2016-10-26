571998b19533c3fe69c6765e719d1f18c43a4875
Per Nyberg <per_skuru@msn.com>
2016-10-26 21:52:39 +0200

    Fixed appearance of A-Air

--- units/AAir.java
+++ units/AAir.java
@@ -28,46 +28,48 @@ public class AAir extends Unit {
 	}
 
 	protected void paintUnit(Graphics g, int tileSize) {
-		int cx1 = x * tileSize + 3 * tileSize / 4;
-		int cy1 = y * tileSize + tileSize / 8;
+		int paintX = x * tileSize;
+		int paintY = y * tileSize;
 
-		int cx2 = x * tileSize + 7 * tileSize / 8;
-		int cy2 = y * tileSize + tileSize / 4;
+		int headAlignX = paintX + tileSize / 3 - 1;
+		int headAlignY = paintY + tileSize / 5;
+		int headWidth = 3 * tileSize / 20 + 2;
+		int headHeight = 4 * tileSize / 20 + 2;
 
-		int cx3 = x * tileSize + 3 * tileSize / 4 - 3;
-		int cy3 = y * tileSize + 7 * tileSize / 20 + 3;
-		int cx4 = x * tileSize + tileSize / 4 + 5;
-		int cy4 = y * tileSize + 7 * tileSize / 20 + 3;
-
+		int bodyAlignX = headAlignX;
+		int bodyAlignY = headAlignY + headHeight;
 		int bodyWidth = 2 * tileSize / 5 + 1;
 		int bodyHeight = tileSize / 4 + 3;
-		int bodyAlignX = tileSize / 3 - 1;
-		int bodyAlignY = 7 * tileSize / 20 + 3;
+
+		// cannon-points
+		int cx1 = paintX + 3 * tileSize / 4;
+		int cy1 = paintY + tileSize / 8;
+		int cx2 = paintX + 7 * tileSize / 8;
+		int cy2 = paintY + tileSize / 4;
+		int cx3 = paintX + 3 * tileSize / 4 - 3;
+		int cy3 = bodyAlignY;
+		int cx4 = headAlignX + headWidth;
+		int cy4 = bodyAlignY;
+		int cx5 = headAlignX + headWidth;
+		int cy5 = bodyAlignY - 4;
 
 		// cannon
-		int[] cannonX = {cx1, cx2, cx3, cx4};
-		int[] cannonY = {cy1, cy2, cy3, cy4};
-		int npoints = 4;
+		int[] cannonX = {cx1, cx2, cx3, cx4, cx5};
+		int[] cannonY = {cy1, cy2, cy3, cy4, cy5};
+		int npoints = 5;
 
 		if (active) {
 			g.setColor(color);
 		} else {
 			g.setColor(restingColor);
 		}
-		g.fillPolygon(cannonX, cannonY, npoints);
+		g.fillRect(headAlignX, headAlignY, headWidth, headHeight); // head
+		g.fillRect(bodyAlignX, bodyAlignY, bodyWidth, bodyHeight); // body
+		g.fillPolygon(cannonX, cannonY, npoints); // cannon
 
 		g.setColor(Color.black);
-		g.drawPolygon(cannonX, cannonY, npoints);
-
-		// body
-		if (active) {
-			g.setColor(color);
-		} else {
-			g.setColor(restingColor);
-		}
-		g.fillRect(x * tileSize + bodyAlignX, y * tileSize + bodyAlignY, bodyWidth, bodyHeight);
-
-		g.setColor(Color.black);
-		g.drawRect(x * tileSize + bodyAlignX, y * tileSize + bodyAlignY, bodyWidth, bodyHeight);
+		g.drawRect(headAlignX, headAlignY, headWidth, headHeight); // head
+		g.drawRect(bodyAlignX, bodyAlignY, bodyWidth, bodyHeight); // body
+		g.drawPolygon(cannonX, cannonY, npoints); // cannon
 	}
 }
