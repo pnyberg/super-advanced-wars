87173a7eb7cee44f012c20bc7f05b8ffeb2b1945
P Nyberg <per_skuru@msn.com>
2016-12-18 20:26:40 +0100

    Made power-stars fillable (with quarter-steps)

--- graphics/PowerStar.java
+++ graphics/PowerStar.java
@@ -7,15 +7,15 @@ public class PowerStar {
 	public static final int smallSize = 16;
 	public static final int bigSize = 24;
 
-	public static void paintNormal(Graphics g, int x, int y) {
-		paint(g, x, y, smallSize);
+	public static void paintNormal(Graphics g, int x, int y, double filled) {
+		paint(g, x, y, smallSize, filled);
 	}
 
-	public static void paintSuper(Graphics g, int x, int y) {
-		paint(g, x, y, bigSize);
+	public static void paintSuper(Graphics g, int x, int y, double filled) {
+		paint(g, x, y, bigSize, filled);
 	}
 
-	private static void paint(Graphics g, int x, int y, int size) {
+	private static void paint(Graphics g, int x, int y, int size, double filled) {
 		int width = size;
 		int height = size;
 
@@ -56,6 +56,52 @@ public class PowerStar {
 
 		g.setColor(Color.white);
 		g.fillPolygon(cx, cy, n);
+
+		g.setColor(Color.orange);
+		if (filled == 1) {
+			g.fillPolygon(cx, cy, n);
+		} else if (filled >= 0.75) {
+			// right upper limit
+			int xd3 = x4+(x3-x4)/3;
+			int yd3 = y3+2*(y4-y3)/3;
+
+			// left upper limit
+			int xd9 = x9+2*(x8-x9)/3;
+			int yd9 = y9+2*(y8-y9)/3;
+
+			int[] dx = {xd3, x4, x5, x6, x7, x8, xd9};
+			int[] dy = {yd3, y4, y5, y6, y7, y8, yd9};
+			g.fillPolygon(dx, dy, 7);
+		} else if (filled >= 0.5) {
+			// right upper limit
+			int xd4 = x4+(x5-x4)/10;
+			int yd4 = y4+(y5-y4)/10;
+
+			// left upper limit
+			int xd8 = x7+9*(x8-x7)/10;
+			int yd8 = y7+9*(y8-y7)/10;
+
+			int[] dx = {xd4, x5, x6, x7, xd8};
+			int[] dy = {yd4, y5, y6, y7, yd8};
+			g.fillPolygon(dx, dy, 5);
+		} else if (filled >= 0.25) {
+			// right point right upper limit
+			int xd4 = x4+4*(x5-x4)/10;
+			int yd4 = y4+4*(y5-y4)/10;
+
+			// right point left upper limit
+
+			// left point left upper limit
+			int xd8 = x7+6*(x8-x7)/10;
+			int yd8 = y7+6*(y8-y7)/10;
+
+			// left point right upper limit
+
+			int[] dx = {xd4, x5, x6, x7, xd8, x6};
+			int[] dy = {yd4, y5, y6, y7, yd8, y6};
+			g.fillPolygon(dx, dy, 5);
+		}
+		
 		g.setColor(Color.black);
 		g.drawPolygon(cx, cy, n);
 	}
