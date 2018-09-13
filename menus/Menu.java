8a68d7051097a164295396b9cf5ed55e39f04009
Your Name <you@example.com>
2018-09-13 22:26:17 +0200

    Modify Menu

--- menus/Menu.java
+++ menus/Menu.java
@@ -3,10 +3,17 @@ package menus;
 import java.awt.Color;
 import java.awt.Graphics;
 
+import menus.icons.MenuArrow;
+
 public abstract class Menu {
+	protected int x;
+	protected int y;
+	protected int menuIndex;
+	protected int tileSize;
+	protected int menuWidth;
 	protected boolean visible;
-	protected int x, y, menuIndex, numberOfRows, tileSize;
-	protected int menuWidth, menuHeight;
+	
+	private MenuArrow arrow;
 
 	protected final int menuRowHeight = 15;
 	protected final int xAlign = 4;
@@ -17,18 +24,15 @@ public abstract class Menu {
 		y = 0;
 		menuIndex = 0;
 		this.tileSize = tileSize;
-		visible = false;
-		numberOfRows = 0;
-
 		menuWidth = (tileSize * 5 / 3);
-		menuHeight = 0; // is set when it's used
+		visible = false;
+		arrow = new MenuArrow(tileSize, menuRowHeight, yAlign);
 	}
 
 	public void openMenu(int x, int y) {
 		this.x = x;
 		this.y = y;
 		visible = true;
-		updateNumberOfRows();
 	}
 
 	public void closeMenu() {
@@ -37,22 +41,16 @@ public abstract class Menu {
 	}
 
 	public void moveArrowUp() {
-		updateNumberOfRows();
-
 		if (menuIndex > 0) {
 			menuIndex--;
 		}
 	}
 
 	public void moveArrowDown() {
-		updateNumberOfRows();
-
-		if (menuIndex < (numberOfRows - 1)) {
+		if (menuIndex < (getNumberOfRows() - 1)) {
 			menuIndex++;
 		}
 	}
-
-	protected abstract void updateNumberOfRows();
 
 	public int getX() {
 		return x;
@@ -65,14 +63,19 @@ public abstract class Menu {
 	public int getMenuIndex() {
 		return menuIndex;
 	}
+	
+	public abstract int getNumberOfRows();
 
 	public boolean isVisible() {
 		return visible;
 	}
+	
+	public boolean atEndRow() {
+		return menuIndex == getNumberOfRows()-1;
+	}
 
 	protected void paintMenuBackground(Graphics g) {
-		updateNumberOfRows();
-
+		int menuHeight = getNumberOfRows() * menuRowHeight + 10;
 		int menuX = x * tileSize + tileSize / 2;
 		int menuY = y * tileSize + tileSize / 2;
 
@@ -86,34 +89,6 @@ public abstract class Menu {
 	}
 
 	protected void paintArrow(Graphics g) {
-		int arrowWidth = tileSize / 2;
-
-		int menuX = x * tileSize + tileSize / 2;
-		int menuY = y * tileSize + tileSize / 2;
-
-		int ax1 = menuX + 1 - arrowWidth;
-		int ax2 = menuX + 1 - (2 * arrowWidth) / 5;
-		int ax3 = menuX + 1 - (2 * arrowWidth) / 5;
-		int ax4 = menuX + 1;
-		int ax5 = menuX + 1 - (2 * arrowWidth) / 5;
-		int ax6 = menuX + 1 - (2 * arrowWidth) / 5;
-		int ax7 = menuX + 1 - arrowWidth;
-
-		int ay1 = menuY + yAlign + 3 + menuRowHeight * menuIndex;
-		int ay2 = menuY + yAlign + 3 + menuRowHeight * menuIndex;
-		int ay3 = menuY + 1 + menuRowHeight * menuIndex;
-		int ay4 = menuY + 2 + yAlign + menuRowHeight / 2 + menuRowHeight * menuIndex;
-		int ay5 = menuY + 3 + yAlign * 2 + menuRowHeight * (menuIndex + 1);
-		int ay6 = menuY + 1 + yAlign + menuRowHeight * (menuIndex + 1);
-		int ay7 = menuY + 1 + yAlign + menuRowHeight * (menuIndex + 1);
-
-		int[] ax = {ax1, ax2, ax3, ax4, ax5, ax6, ax7};
-		int[] ay = {ay1, ay2, ay3, ay4, ay5, ay6, ay7};
-
-		g.setColor(Color.orange);
-		g.fillPolygon(ax, ay, 7);
-
-		g.setColor(Color.black);
-		g.drawPolygon(ax, ay, 7);
+		arrow.paint(g, x, y, menuIndex);
 	}
 }
