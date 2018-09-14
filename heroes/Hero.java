eaf851c2ba16c01a02ad8801f98591dbabad6f30
Your Name <you@example.com>
2018-09-14 11:24:50 +0200

    Modify Hero

--- heroes/Hero.java
+++ heroes/Hero.java
@@ -8,34 +8,17 @@ import java.util.ArrayList;
 
 public class Hero {
 	private int cash;
-	private int attackValue, defenceValue;
-	private double starPower;
-	private int powerAmount, superPowerAmount;
 	private Color color;
-	private ArrayList<Unit> troops;
+	private AttackDefenceObject attackDefenceObject;
+	private HeroPowerMeter heroPowerMeter;
+	private TroopHandler troopHandler;
 
-	public Hero(int heroIndex) {
+	public Hero(Color color, AttackDefenceObject attackDefenceObject, HeroPowerMeter heroPowerMeter) {
 		cash = 0;
-		starPower = 0;
-		troops = new ArrayList<Unit>();
-
-		initHero(heroIndex);
-	}
-
-	private void initHero(int heroIndex) {
-		if (heroIndex == 0) {
-			attackValue = 100;
-			defenceValue = 100;
-			powerAmount = 3;
-			superPowerAmount = 6;
-			color = Color.red; // magenta
-		} else if (heroIndex == 1) {
-			attackValue = 110;
-			defenceValue = 110;
-			powerAmount = 4;
-			superPowerAmount = 7;
-			color = Color.orange;
-		}
+		this.color = color;
+		this.attackDefenceObject = attackDefenceObject;
+		this.heroPowerMeter = heroPowerMeter;
+		troopHandler = new TroopHandler();
 	}
 
 	public void manageCash(int cashDiff) {
@@ -48,39 +31,6 @@ public class Hero {
 		}
 	}
 
-	public void addTroop(Unit unit) {
-		troops.add(unit);
-	}
-
-	public void removeTroop(Unit unit) {
-		for (int i = 0 ; i < troops.size() ; i++) {
-			if (troops.get(i) == unit) {
-				troops.remove(i);
-				break;
-			}
-		}
-	}
-	
-	public void addStarPower(double value) {
-		starPower += value;
-	}
-
-	public int getAttackValue(int unitIndex) {
-		return attackValue;
-	}
-
-	public int getDefenceValue(int unitIndex) {
-		return defenceValue;
-	}
-
-	public Unit getTroop(int index) {
-		return troops.get(index);
-	}
-
-	public int getTroopSize() {
-		return troops.size();
-	}
-
 	public int getCash() {
 		return cash;
 	}
@@ -88,25 +38,17 @@ public class Hero {
 	public Color getColor() {
 		return color;
 	}
-
-	public double getStarPower() {
-		return starPower;
-	}
-
-	public int getRequiredPower() {
-		return powerAmount;
-	}
-
-	public int getRequiredSuperPower() {
-		return superPowerAmount;
+	
+	public AttackDefenceObject attackDefenceObject() {
+		return attackDefenceObject;
 	}
 	
-	public boolean powerUsable() {
-		return starPower >= powerAmount;
+	public HeroPowerMeter getHeroPowerMeter() {
+		return heroPowerMeter;
 	}
-
-	public boolean superPowerUsable() {
-		return starPower >= superPowerAmount;
+	
+	public TroopHandler geTroopHandler() {
+		return troopHandler;
 	}
 
 	public void paint(Graphics g, int x, int y) {
