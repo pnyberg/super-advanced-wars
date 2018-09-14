755731661c1bc79c2b13da8d5cb9b9b61b4c3400
Your Name <you@example.com>
2018-09-14 11:30:01 +0200

    Add HeroFactory

---
+++ heroes/HeroFactory.java
@@ -0,0 +1,23 @@
+package heroes;
+
+import java.awt.Color;
+
+public class HeroFactory {
+
+	public HeroFactory() {
+		
+	}
+	
+	public Hero createHero(int heroIndex) {
+		if (heroIndex == 0) {
+			AttackDefenceObject attackDefenceObject = new AttackDefenceObject(100, 100);
+			HeroPowerMeter heroPowerMeter = new HeroPowerMeter(3, 6);
+			return new Hero(Color.red, attackDefenceObject, heroPowerMeter);
+		} else if (heroIndex == 1) {
+			AttackDefenceObject attackDefenceObject = new AttackDefenceObject(110, 110);
+			HeroPowerMeter heroPowerMeter = new HeroPowerMeter(4, 7);
+			return new Hero(Color.orange, attackDefenceObject, heroPowerMeter);
+		}
+		return null;
+	}
+}
