26fece7befa709d42433f0d7680081e18469ca0d
Your Name <you@example.com>
2018-09-14 11:18:47 +0200

    Add AttackDefenceObject

---
+++ heroes/AttackDefenceObject.java
@@ -0,0 +1,19 @@
+package heroes;
+
+public class AttackDefenceObject {
+	private int attackValue;
+	private int defenceValue;
+	
+	public AttackDefenceObject(int attackValue, int defenceValue) {
+		this.attackValue = attackValue;
+		this.defenceValue = defenceValue;
+	}
+	
+	public int getAttackValue(int unitIndex) {
+		return attackValue;
+	}
+
+	public int getDefenceValue(int unitIndex) {
+		return defenceValue;
+	}
+}
