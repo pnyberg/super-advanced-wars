bfc37a5dbc4d6208a0d63e120b2065400aac9492
Your Name <you@example.com>
2018-09-15 02:01:27 +0200

    Add DimensionValues

---
+++ menus/DimensionValues.java
@@ -0,0 +1,32 @@
+/**
+ * TODO: make into a Builder-class
+ */
+package menus;
+
+public class DimensionValues {
+	public int tileSize;
+	public int xAlign;
+	public int yAlign;
+	public int menuRowWidth;
+	public int menuRowHeight;
+	
+	public int getTileSize() {
+		return tileSize;
+	}
+	
+	public int getAlignX() {
+		return xAlign;
+	}
+	
+	public int getAlignY() {
+		return yAlign;
+	}
+	
+	public int getMenuRowWidth() {
+		return menuRowWidth;
+	}
+	
+	public int getMenuRowHeight() {
+		return menuRowHeight;
+	}
+}
