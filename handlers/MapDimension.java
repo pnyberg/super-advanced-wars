6bd9a9711e31fc0b276370a59adbd0fbd92bc771
Your Name <you@example.com>
2018-09-17 20:55:56 +0200

    Add MapDimension

---
+++ handlers/MapDimension.java
@@ -0,0 +1,11 @@
+package handlers;
+
+public class MapDimension {
+	public final int width;
+	public final int height;
+	
+	public MapDimension(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+}
