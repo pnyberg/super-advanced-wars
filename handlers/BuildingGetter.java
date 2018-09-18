e8334afd93da0782842d2236e7bdf24e76616610
Your Name <you@example.com>
2018-09-18 22:14:25 +0200

    Add BuildingGetter

---
+++ handlers/BuildingGetter.java
@@ -0,0 +1,36 @@
+package handlers;
+
+import java.util.ArrayList;
+
+import area.buildings.Building;
+import heroes.HeroPortrait;
+
+public class BuildingGetter {
+	private HeroPortrait portrait;
+	private ArrayList<Building> buildings;
+	
+	public BuildingGetter(HeroPortrait portrait, ArrayList<Building> buildings) {
+		this.buildings = buildings;
+		this.portrait = portrait;
+	}
+
+	public Building getBuilding(int x, int y) {
+		for (Building building : buildings) {
+			if (building.getX() == x && building.getY() == y) {
+				return building;
+			}
+		}
+
+		return null;
+	}
+
+	public Building getFriendlyBuilding(int x, int y) {
+		Building building = getBuilding(x, y);
+
+		if (building != null && building.getOwner() == portrait.getCurrentHero()) {
+			return building;
+		}
+
+		return null;
+	}
+}
