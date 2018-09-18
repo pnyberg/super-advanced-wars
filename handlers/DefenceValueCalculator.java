6ee521b10356bc35e3f2ef82bd3aa0a43e801cef
Your Name <you@example.com>
2018-09-18 21:09:17 +0200

    Add DefenceValueCalculator

---
+++ handlers/DefenceValueCalculator.java
@@ -0,0 +1,31 @@
+package handlers;
+
+import area.TerrainType;
+
+public class DefenceValueCalculator {
+
+	public DefenceValueCalculator() {
+		
+	}
+	
+	public int getDefenceValue(TerrainType terrainType) {
+		if (terrainType == TerrainType.ROAD ||
+			terrainType == TerrainType.SEA ||
+			terrainType == TerrainType.SHORE) {
+			return 0;
+		} else if (terrainType == TerrainType.PLAIN ||
+					terrainType == TerrainType.REEF) {
+			return 1;
+		} else if (terrainType == TerrainType.WOOD) {
+			return 2;
+		} else if (terrainType == TerrainType.CITY ||
+					terrainType == TerrainType.FACTORY ||
+					terrainType == TerrainType.AIRPORT ||
+					terrainType == TerrainType.PORT) {
+			return 3;
+		} else if (terrainType == TerrainType.MOUNTAIN) {
+			return 4;
+		} 
+		return -1;
+	}
+}
