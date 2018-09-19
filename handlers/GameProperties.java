307fdb63f62972a23326ffee700428ef18d9e79a
Your Name <you@example.com>
2018-09-20 00:17:09 +0200

    Add GameProperties

---
+++ handlers/GameProperties.java
@@ -0,0 +1,9 @@
+package handlers;
+
+public class GameProperties {
+	public final int fuelMaintenancePerTurn;
+	
+	public GameProperties(int fuelMaintenancePerTurn) {
+		this.fuelMaintenancePerTurn = fuelMaintenancePerTurn;
+	}
+}
