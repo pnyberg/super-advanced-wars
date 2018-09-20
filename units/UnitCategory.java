fdfbc855718e2bc21f041f3349e76055bc46d853
Your Name <you@example.com>
2018-09-20 23:12:01 +0200

    Add UnitCategory

---
+++ units/UnitCategory.java
@@ -0,0 +1,20 @@
+package units;
+
+public enum UnitCategory {
+	FOOTMAN(0),
+	VEHICLE(1),
+	PLANE(2),
+	COPTER(3),
+	BOAT(4),
+	SUB(5);
+
+	private final int unitCategoryIndex;
+
+	UnitCategory(int unitCategoryIndex) {
+		this.unitCategoryIndex = unitCategoryIndex;
+	}
+	
+	public int unitCategoryIndex() {
+		return unitCategoryIndex;
+	}
+}
