adfb4ea61211f7497986913643e5aa0102ba5506
Your Name <you@example.com>
2018-09-20 23:11:36 +0200

    Add AttackType

---
+++ units/AttackType.java
@@ -0,0 +1,17 @@
+package units;
+
+public enum AttackType {
+	NONE(0),
+	DIRECT_ATTACK(1),
+	INDIRECT_ATTACK(2);
+	
+	private int attackTypeIndex;
+
+	AttackType(int attackTypeIndex) {
+		this.attackTypeIndex = attackTypeIndex;
+	}
+	
+	public int attackTypeIndex() {
+		return attackTypeIndex;
+	}
+}
