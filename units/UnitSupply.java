5f0841995aabf799bfc206e5ec707bd04b49994b
Your Name <you@example.com>
2018-09-23 02:52:27 +0200

    Add UnitSupply

---
+++ units/UnitSupply.java
@@ -0,0 +1,52 @@
+package units;
+
+public class UnitSupply {
+	private int maxFuel;
+	private int currentFuel;
+	private int maxAmmo;
+	private int currentAmmo;
+	
+	public UnitSupply(int maxFuel, int maxAmmo) {
+		this.maxFuel = maxFuel;
+		currentFuel = maxFuel;
+		this.maxAmmo = maxAmmo;
+		currentAmmo = maxAmmo;
+	}
+	
+	public void replentish() {
+		currentFuel = maxFuel;
+		currentAmmo = maxAmmo;
+	}
+
+	public void useAmmo() {
+		currentAmmo--;
+	}
+
+	public void useFuel(int fuel) {
+		currentFuel -= fuel;
+	} 
+
+	public boolean hasAmmo() {
+		return currentAmmo > 0;
+	}
+
+	public boolean hasFuel(int fuelNeeded) {
+		return currentFuel >= fuelNeeded;
+	}
+	
+	public int getAmmo() {
+		return currentAmmo;
+	}
+
+	public int getMaxAmmo() {
+		return maxAmmo;
+	}
+
+	public int getFuel() {
+		return currentFuel;
+	}
+
+	public int getMaxFuel() {
+		return maxFuel;
+	}
+}
