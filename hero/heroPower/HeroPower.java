eac2b3ed2be31c3689b88d61d4f400a77cb2ad4d
Your Name <you@example.com>
2018-10-09 23:22:56 +0200

    Add power and super power to heropower

--- hero/heroPower/HeroPower.java
+++ hero/heroPower/HeroPower.java
@@ -1,13 +1,29 @@
 package hero.heroPower;
 
+import java.util.List;
+
+import hero.heroAbilities.CommanderEffect;
+
 public class HeroPower {
 	private HeroPowerMeter heroPowerMeter;
+	private List<CommanderEffect> powerEffects;
+	private List<CommanderEffect> superPowerEffects;
 	
-	public HeroPower(HeroPowerMeter heroPowerMeter) {
+	public HeroPower(HeroPowerMeter heroPowerMeter, List<CommanderEffect> powerEffects, List<CommanderEffect> superPowerEffects) {
 		this.heroPowerMeter = heroPowerMeter;
+		this.powerEffects = powerEffects;
+		this.superPowerEffects = superPowerEffects;
 	}
 	
 	public HeroPowerMeter getHeroPowerMeter() {
 		return heroPowerMeter;
+	}
+	
+	public List<CommanderEffect> getPowerEffects() {
+		return powerEffects;
+	}
+	
+	public List<CommanderEffect> getSuperPowerEffects() {
+		return superPowerEffects;
 	}
 }
