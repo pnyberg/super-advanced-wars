c5f9996caf5835b6dfd404f03a59ec02d6b164a8
Your Name <you@example.com>
2018-10-09 23:21:03 +0200

    Update DefValueCalculator

--- combat/DefenceValueCalculator.java
+++ combat/DefenceValueCalculator.java
@@ -1,6 +1,11 @@
 package combat;
 
+import hero.Hero;
+import hero.heroAbilities.AttackIncrease;
+import hero.heroAbilities.CommanderEffect;
+import hero.heroAbilities.DefenceIncrease;
 import map.area.TerrainType;
+import units.MovementType;
 
 public class DefenceValueCalculator {
 
@@ -8,7 +13,44 @@ public class DefenceValueCalculator {
 		
 	}
 	
-	public int getDefenceValue(TerrainType terrainType) {
+	public int calculateDefenceValue(Hero defendingHero, int defendingUnitIndex) {
+		int normalDefenceValue = defendingHero.getPassiveHeroAbilities().getDefenceValue(defendingUnitIndex);
+		int powerDefenceValue = calculatePowerDefenceValue(defendingHero, defendingUnitIndex);
+		int superPowerDefenceValue = calculateSuperPowerDefenceValue(defendingHero, defendingUnitIndex);;
+		return normalDefenceValue + powerDefenceValue + superPowerDefenceValue;
+	}
+	
+	private int calculatePowerDefenceValue(Hero hero, int unitIndex) {
+		int defenceValue = 0;
+		if (hero.isPowerActive()) {
+			defenceValue = 10;
+			for (CommanderEffect commanderEffect : hero.getHeroPower().getPowerEffects()) {
+				if (commanderEffect instanceof DefenceIncrease) {
+					defenceValue += ((DefenceIncrease)commanderEffect).getDefenceIncrease(unitIndex);
+				}
+			}
+			
+		}
+		return defenceValue;
+	}
+
+	private int calculateSuperPowerDefenceValue(Hero hero, int unitIndex) {
+		int defenceValue = 0;
+		if (hero.isSuperPowerActive()) {
+			defenceValue = 10;
+			for (CommanderEffect commanderEffect : hero.getHeroPower().getSuperPowerEffects()) {
+				if (commanderEffect instanceof DefenceIncrease) {
+					defenceValue += ((DefenceIncrease)commanderEffect).getDefenceIncrease(unitIndex);
+				}
+			}
+		}
+		return defenceValue;
+	}
+
+	public int getTerrainDefenceValue(MovementType defendingMovementType, TerrainType terrainType) {
+		if (defendingMovementType == MovementType.AIR) {
+			return 0;
+		}
 		if (terrainType == TerrainType.ROAD ||
 			terrainType == TerrainType.SEA ||
 			terrainType == TerrainType.SHORE) {
