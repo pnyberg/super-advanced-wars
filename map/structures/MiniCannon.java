bfca04e41900928fe2f49a3cb9ab18e359f00b4b
Your Name <you@example.com>
2018-10-12 23:31:41 +0200

    Implemented all directions (drawings) for minicannons

--- map/structures/MiniCannon.java
+++ map/structures/MiniCannon.java
@@ -18,8 +18,8 @@ public class MiniCannon extends FiringStructure {
 	
 	public void fillRangeMap(boolean[][] rangeMap) {
 		if (direction == Direction.SOUTH) {
-			int rangeX = x/tileSize;
-			int rangeY = y/tileSize + 1;
+			int rangeX = point.getX()/tileSize;
+			int rangeY = point.getY()/tileSize + 1;
 			for (int k = 1 ; k <= 5 ; k += 2) {
 				for (int i = 0 ; i < k ; i++) {
 					rangeMap[rangeX+i][rangeY] = true;
@@ -40,15 +40,65 @@ public class MiniCannon extends FiringStructure {
 		
 	public void paint(Graphics g) {
 		g.setColor(new Color(204,204,0)); // darker yellow
-		g.fillRect(x, y, tileSize, tileSize);
+		g.fillRect(point.getX(), point.getY(), tileSize, tileSize);
 		g.setColor(Color.black);
-		g.drawRect(x, y, tileSize, tileSize);
-		g.setColor(owner.getColor());
+		g.drawRect(point.getX(), point.getY(), tileSize, tileSize);
+		
+		setCannonColor(g);
+		if (direction == Direction.NORTH) {
+			paintNorthMiniCannon(g);
+		} else if (direction == Direction.EAST) {
+			paintEastMiniCannon(g);
+		} else if (direction == Direction.SOUTH) {
+			paintSouthMiniCannon(g);
+		} else if (direction == Direction.WEST) {
+			paintWestMiniCannon(g);
+		}
+	}
+	
+	private void setCannonColor(Graphics g) {
+		if (owner == null) {
+			g.setColor(Color.darkGray);
+		} else {
+			g.setColor(owner.getColor());
+		}
+	}
+	
+	private void paintNorthMiniCannon(Graphics g) {
+		g.fillOval(point.getX()+3*tileSize/10, point.getY()+tileSize/20, 2 * tileSize / 5, 2 * tileSize / 5);
+		g.setColor(Color.black);
+		g.drawOval(point.getX()+3*tileSize/10, point.getY()+tileSize/20, 2 * tileSize / 5, 2 * tileSize / 5);
+		setCannonColor(g);
+		g.fillOval(point.getX()+tileSize/8, point.getY()+tileSize/8, 3 * tileSize / 4, 6 * tileSize / 8);
+		g.setColor(Color.black);
+		g.drawOval(point.getX()+tileSize/8, point.getY()+tileSize/8, 3 * tileSize / 4, 6 * tileSize / 8);
+	}
 
-		g.fillOval(x+tileSize/8, y+tileSize/16, 3 * tileSize / 4, 7 * tileSize / 8);
+	private void paintEastMiniCannon(Graphics g) {
+		g.fillOval(point.getX()+5*tileSize/10, point.getY()+tileSize/5, 1 * tileSize / 5, 2 * tileSize / 5);
 		g.setColor(Color.black);
-		g.drawOval(x+tileSize/8, y+tileSize/16, 3 * tileSize / 4, 7 * tileSize / 8);
-		g.drawOval(x+3*tileSize/10, y+tileSize/5, 2 * tileSize / 5, 2 * tileSize / 5);
-		g.fillOval(x+4*tileSize/10, y+3*tileSize/10, tileSize / 5, tileSize / 5);
+		g.drawOval(point.getX()+5*tileSize/10, point.getY()+tileSize/5, 1 * tileSize / 5, 2 * tileSize / 5);
+		setCannonColor(g);
+		g.fillOval(point.getX()+tileSize/8, point.getY()+tileSize/16, 2 * tileSize / 4, 7 * tileSize / 8);
+		g.setColor(Color.black);
+		g.drawOval(point.getX()+tileSize/8, point.getY()+tileSize/16, 2 * tileSize / 4, 7 * tileSize / 8);
+	}
+
+	private void paintSouthMiniCannon(Graphics g) {
+		g.fillOval(point.getX()+tileSize/8, point.getY()+tileSize/16, 3 * tileSize / 4, 7 * tileSize / 8);
+		g.setColor(Color.black);
+		g.drawOval(point.getX()+tileSize/8, point.getY()+tileSize/16, 3 * tileSize / 4, 7 * tileSize / 8);
+		g.drawOval(point.getX()+3*tileSize/10, point.getY()+tileSize/5, 2 * tileSize / 5, 2 * tileSize / 5);
+		g.fillOval(point.getX()+4*tileSize/10, point.getY()+3*tileSize/10, tileSize / 5, tileSize / 5);
+	}
+
+	private void paintWestMiniCannon(Graphics g) {
+		g.fillOval(point.getX()+3*tileSize/10, point.getY()+tileSize/5, 1 * tileSize / 5, 2 * tileSize / 5);
+		g.setColor(Color.black);
+		g.drawOval(point.getX()+3*tileSize/10, point.getY()+tileSize/5, 1 * tileSize / 5, 2 * tileSize / 5);
+		setCannonColor(g);
+		g.fillOval(point.getX()+3*tileSize/8, point.getY()+tileSize/16, 2 * tileSize / 4, 7 * tileSize / 8);
+		g.setColor(Color.black);
+		g.drawOval(point.getX()+3*tileSize/8, point.getY()+tileSize/16, 2 * tileSize / 4, 7 * tileSize / 8);
 	}
 }
